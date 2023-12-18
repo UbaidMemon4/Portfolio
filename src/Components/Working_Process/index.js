0ea88046991f9269a03923142b32ef95fb6f28d4
Ubaid <ubaidasif510%gmail.com>
2023-12-18 03:39:49 -0800

    final complete

--- src/Components/Working_Process/index.js
+++ src/Components/Working_Process/index.js
@@ -5,9 +5,11 @@ import { PlayCircleOutlined } from "@ant-design/icons";
 const Working_Process = () => {
   return (
     <div className="working_bg">
+      <div className="play_img">
       <PlayCircleOutlined className="play_logo" />
       <h1 className="working">Working Process</h1>
       <p className="working_para">Now I Made Practice Projects</p>
+      </div>
     </div>
   );
 };
